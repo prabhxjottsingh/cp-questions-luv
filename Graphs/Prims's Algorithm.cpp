0759ff1908d97eb16c91f12027efa8624691840e
Prabhxjott <prabhx.jott@gmail.com>
2022-07-08 20:01:17 +0530

    Graphs Algos

---
+++ Graphs/Prims's Algorithm.cpp
@@ -0,0 +1,6 @@
+/*
+Its basically brute Force:
+it picks up the node (out if all the currently presetn nodes in  the minimum spanning tree) with smallest weight and adds to it
+thus increasing the size of MST by one,
+at the point MST contains all the nodes of the graph the algo then ends
+*/
